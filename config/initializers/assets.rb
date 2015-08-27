79e0e98cfc71192cf8151f9ba03c42a5f9ef0bcb
adrian <adrian.palomera@punsset.com>
2015-08-27 16:20:22 +0200

    removed dropzone

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # gestion.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( dropzone/* frontend.css frontend.js gestion.js gestion.css)
+Rails.application.config.assets.precompile += %w( frontend.css frontend.js gestion.js gestion.css)
